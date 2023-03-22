c5e77840d6e1997489d45fba24ecd6b189df8b95
AndriiSotnikov <andrii.sotnikov@wtt-solution.com>
2023-03-22 20:39:29 +0100

    connect redux with components

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,14 +1,32 @@
-import data from '../mock/data.json';
-import { JobPosition } from './JobPosition';
+import { JobPosition } from './JobPosition'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectVisiblePositions } from '../store/positions/positions-selectors'
 
+import { selectFilters } from 'store/filter/filter-selectors'
+
+import { addFilter } from 'store/filter/filter-actions'
 const JobList = () => {
+  const dispatch = useDispatch()
+  const currentFilter = useSelector(selectFilters)
+  const positions = useSelector((state) =>
+    selectVisiblePositions(state, currentFilter)
+  )
+
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter))
+  }
   return (
     <div className='job-list'>
-      {data.map(item => (
-        <JobPosition key={item.id} {...item} />
+      {positions.map((item) => (
+        <JobPosition
+          key={item.id}
+          {...item}
+          handleAddFilter={handleAddFilter}
+          onClick
+        />
       ))}
     </div>
   )
 }
 
-export {JobList};
+export { JobList }
