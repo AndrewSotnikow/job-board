1123a9412ec78145d6982151b4aed0dd47bc3010
AndriiSotnikov <andrii.sotnikov@wtt-solution.com>
2023-03-22 21:59:43 +0100

    add redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,21 @@
 import { createStore } from 'redux'
 import { rootReducer } from './root-reducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-export const store = createStore(
-  rootReducer,
+const persistConfig = {
+  key: 'root',
+  storage,
+  // whitelist: ['filter', 'position] will add this reducers to storage
+  // blacklist: ['filter', 'position] will not add this reducers to storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(
+  persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+export { store }
+export const persistor = persistStore(store)
